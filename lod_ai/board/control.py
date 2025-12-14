28c033de67a5324e4482245c1da7f344aac93801
FeeSimple1 <ecommentation@gmail.com>
2025-12-14 11:58:34 -0600

    Delete lod_ai/board/control.py

--- lod_ai/board/control.py
+++
@@ -1,7 +0,0 @@
-"""
-shim: re-export refresh_control so
-    from lod_ai.board.control import refresh_control
-continues to work.
-"""
-
-from lod_ai.map.control import refresh_control   # re-export
