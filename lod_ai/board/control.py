a308b3bc9d62ff78fcd229a8e1dbe86b005a88b5
FeeSimple1 <ecommentation@gmail.com>
2025-12-14 12:12:48 -0600

    Add files via upload

---
+++ lod_ai/board/control.py
@@ -0,0 +1,7 @@
+"""
+shim: re-export refresh_control so
+    from lod_ai.board.control import refresh_control
+continues to work.
+"""
+
+from lod_ai.map.control import refresh_control   # re-export
