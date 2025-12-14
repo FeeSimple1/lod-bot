875992fac557b8a96c41f499e8de7f8b232f2a21
FeeSimple1 <ecommentation@gmail.com>
2025-12-14 12:06:08 -0600

    Delete lod_ai/util directory

--- lod_ai/util/adjacency.py
+++
@@ -1,9 +0,0 @@
-"""
-shim: keep old imports working
-
-    from lod_ai.util.adjacency import is_adjacent
-
-while the real function now lives in lod_ai.map.adjacency.
-"""
-
-from lod_ai.map.adjacency import is_adjacent  # re-export
